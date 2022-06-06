59957a86192c40442736fe4c9ae4286cd8cd2cc5
Royce Chua <royce236@gmail.com>
2022-06-06 12:35:42 +0800

    Add models

---
+++ src/models/weather.ts
@@ -0,0 +1,55 @@
+
+type Coordinate = {
+    "lat": number,
+    "lon": number,
+}
+
+type Cloud = {
+    "all": number,
+}
+
+type Main = {
+    "feels_like": number,
+    "humidity": number,
+    "pressure": number,
+    "temp": number,
+    "temp_max": number,
+    "temp_min": number,
+}
+
+type Sys = {
+    "country": string,
+    "id": number,
+    "sunrise": number,
+    "sunset": number,
+    "type": number,
+}
+
+type Wind = {
+    "deg": number,
+    "gust": number,
+    "speed": number,
+}
+
+type Weather = {
+    "description": string,
+    "icon": string,
+    "id": number,
+    "main": string,
+  }
+
+type WeatherInfo = {
+    "base": string,
+    "clouds": Cloud,
+    "cod": number,
+    "coord": Coordinate,
+    "dt": number,
+    "id": number,
+    "main": Main,
+    "name": string,
+    "sys": Sys,
+    "timezone": 28800,
+    "visibility": 10000,
+    "weather": Weather[],
+    "wind": Wind,
+}
